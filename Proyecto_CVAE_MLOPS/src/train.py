b5f901c08a59c2aecba76c7276ff0dc1a72ac311
Javier Díaz-Cano <163293850+Javuchenn@users.noreply.github.com>
2026-05-03 17:17:43 +0200

    añadir módulos principales y archivo de config
    - Añadir main.py, model.py, train.py y utils.py en src/
    - Añadir config.yaml en config/

---
+++ Proyecto_CVAE_MLOPS/src/train.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+def train_model(model, X_train, y_train, X_val, y_val, epochs, batch_size, shuffle, early_stopping_patience, early_stopping_monitor, model_path):
+
+    early_stop = EarlyStopping(
+        monitor=early_stopping_monitor,
+        patience=early_stopping_patience,
+        restore_best_weights=True            # Con esto el modelo final queda con los pesos del epoch que tuvo mejor val_loss
+    )
+
+    checkpoint = ModelCheckpoint(
+        model_path,                          # Se guarda el mejor modelo
+        monitor=early_stopping_monitor,
+        save_best_only=True,
+        mode='min',
+        verbose=1
+    )
+
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_val, y_val),
+        epochs=epochs,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        verbose=1,
+        callbacks=[early_stop, checkpoint]
+    )
+
+    return model, history
